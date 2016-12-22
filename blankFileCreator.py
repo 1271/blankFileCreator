1869712ab0b6719c244319955d21eb367f9a2394
sttv <sttv-pc@mail.ru>
2016-12-22 23:09:42 +0300

    refactoryng

--- blankFileCreator.py
+++ blankFileCreator.py
@@ -2,25 +2,26 @@
 import sys
 import argparse
 
-def createParser ():
-  parser = argparse.ArgumentParser()
 
-  parser.add_argument("-s", "--size", type=int, required=True, help="size in megabytes (if -g, then gigabytes)")
-  parser.add_argument("-f", "--file", required=True, help="file name")
-  parser.add_argument("-g", "--gigabytes", action='store_const', const=True, default=False)
+def create_parser():
+    parse = argparse.ArgumentParser()
 
-  return parser
+    parse.add_argument("-s", "--size", type=int, required=True, help="size in megabytes (if -g, then gigabytes)")
+    parse.add_argument("-f", "--file", required=True, help="file name")
+    parse.add_argument("-g", "--gigabytes", action='store_const', const=True, default=False)
 
-parser = createParser()
+    return parse
+
+parser = create_parser()
 ns = parser.parse_args()
 
 if (ns.size < 1) or (ns.size > 9999999):
-  print("Invalid parameter! Allowed only the numbers 1 to 9999999")
-  sys.exit(1)
+    print("Invalid parameter! Allowed only the numbers 1 to 9999999")
+    sys.exit(1)
 
 size = ns.size
 if ns.gigabytes:
-  size = size * 1024
+    size *= 1024
 
 f=open(ns.file, "w")
 f.seek(1024 * 1024 * size)
